861c438a438c568ca737f686b51aee5371f0139e
Andrew Doyle <droodoyle89@gmail.com>
2017-08-01 17:08:14 -0400

    Add files via upload

---
+++ scripts/blue tank.gd
@@ -0,0 +1,40 @@
+extends AnimatedSprite
+
+var HP = 10
+
+func _ready():
+	# connect the event for _on_click to listen for when the soldier is clicked on
+	#get_node("Area2D").connect("pressed", self, "_on_click")
+	add_to_group("units")
+	add_to_group("blue")
+	get_parent().unitLocations[get_location()] = get_name()
+	set_process(true)
+
+func move():
+	var pre_move_location = get_location()
+	var mouse = get_viewport().get_mouse_pos()
+	mouse.x = int(mouse.x/16)*16+8
+	mouse.y = int(mouse.y/16)*16+8
+	if(mouse.x < get_viewport_rect().size.x and mouse.y < get_viewport_rect().size.y):
+		set_pos(mouse)
+		get_parent().get_node("Timer").start()
+		yield(get_parent().get_node("Timer"), "timeout")
+	command_menu()
+
+func get_location():
+	return((int(get_pos().x/16)*16)/16+((int(get_pos().y/16)*16)/16)*15)
+
+func consider_moving():
+	#(animate the soldier while he is getting ready to move)
+	#highlight walkable area: call for the consider moving tiles to appear
+	get_node("UnitMaps/HighlightMap").set_opacity(0.6)
+	#higlight attackable enemies
+
+
+func command_menu():
+	#bring up the command menu
+	get_node("Command List").popup()
+
+func _on_Hitbox_input_event( viewport, event, shape_idx ):
+	if(event.type == InputEvent.MOUSE_BUTTON and event.is_pressed()):
+		consider_moving()
---
+++ scripts/neutralFactory.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var HP = 20
+var my_color = null
+
+func _ready():
+	add_to_group("buildings")
+	add_to_group("factories")
+	get_parent().buildingLocations[get_location()] = get_name()
+	
+func get_location():
+	return((int(get_pos().x/16)*16)/16+((int(get_pos().y/16)*16)/16)*15)
+
+func capture(var damage, var attacker_color):
+	HP -= damage
+	if(HP>0):
+		get_node("HP").set_text(str(HP))
+	else:
+		get_node("HP").set_text("")
+		HP = 20
+		# most of this is generalized now, but we still have to set up support for the building changing to the attacker's color building, not just the red one
+		get_parent().set_cell((int(get_pos().x/16)*16)/16, (int(get_pos().y/16)*16)/16, 13)
+		add_to_group(attacker_color)
+		if(my_color!=null):
+			remove_from_group(my_color)
+		my_color = attacker_color
