613ec01c949853d8c6efe07311621e357babaaeb
Andrew Doyle <droodoyle89@gmail.com>
2017-08-01 17:12:58 -0400

    Added a directory for Godot scripts.

--- consider moving.gd
+++
@@ -1,7 +0,0 @@
-extends TileMap
-
-func _on_Area2D_input_event( viewport, event, shape_idx ):
-	if(event.type == InputEvent.MOUSE_BUTTON and !event.is_pressed() and get_opacity() == 0.6):
-		set_opacity(0)
-		get_parent().get_parent().move()
-		#get_parent().move()
